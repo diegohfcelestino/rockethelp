aea2d20951827cd33e7be8694bdf83fdd15ea99c
diegohfcelestino <diegohfcelestino@gmail.com>
2022-07-20 11:52:27 -0300

    Salvando solicitações no banco de dados

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import auth from "@react-native-firebase/auth";
+import { Alert } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import {
   HStack,
@@ -40,6 +42,15 @@ export function Home() {
     navigation.navigate("details", { orderId });
   }
 
+  function handleLogout() {
+    auth()
+      .signOut()
+      .catch(error => {
+        console.log(error);
+        return Alert.alert("Sair", "Não foi possível sair");
+      });
+  }
+
   return (
     <VStack flex={1} pb={6} bg="gray.700">
       <HStack
@@ -52,7 +63,10 @@ export function Home() {
         px={6}
       >
         <Logo />
-        <IconButton icon={<SignOut size={26} color={colors.gray[300]} />} />
+        <IconButton
+          icon={<SignOut size={26} color={colors.gray[300]} />}
+          onPress={handleLogout}
+        />
       </HStack>
 
       <VStack flex={1} px={6}>
