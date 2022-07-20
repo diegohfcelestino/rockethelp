e145cad03798586414ae14bde5d7b9773bffef48
diegohfcelestino <diegohfcelestino@gmail.com>
2022-07-20 14:51:48 -0300

    Criando componente Detail

--- src/screens/Details.tsx
+++ src/screens/Details.tsx
@@ -1,18 +1,90 @@
-import { VStack, Text } from "native-base";
+import { useState, useEffect } from "react";
+import auth from "@react-native-firebase/auth";
+import firestore from "@react-native-firebase/firestore";
+import { Alert } from "react-native";
+import { VStack, useTheme, HStack, Text } from "native-base";
 import { useRoute } from "@react-navigation/native";
 import { Header } from "../components/Header";
+import { OrderProps } from "../components/Order";
+import { OrderFirestoreDTO } from "../DTOs/OrderFirestoreDTO";
+import { dateFormat } from "../utils/firestoreDateFormat";
+import { Loading } from "../components/Loading";
+import { CircleWavyCheck, Hourglass } from "phosphor-react-native";
 
 type RoutesParams = {
   orderId: string;
 };
 
+type OrderDetails = OrderProps & {
+  description: string;
+  solution: string;
+  closed: string;
+};
+
 export function Details() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [solution, setSolution] = useState("");
+  const [order, SetOrder] = useState<OrderDetails>({} as OrderDetails);
+
+  const { colors } = useTheme();
   const route = useRoute();
   const { orderId } = route.params as RoutesParams;
+
+  useEffect(() => {
+    firestore()
+      .collection<OrderFirestoreDTO>("orders")
+      .doc(orderId)
+      .get()
+      .then(doc => {
+        const {
+          patrimony,
+          description,
+          status,
+          created_at,
+          closed_at,
+          solution
+        } = doc.data();
+
+        const closed = closed_at ? dateFormat(closed_at) : null;
+
+        SetOrder({
+          id: doc.id,
+          patrimony,
+          description,
+          status,
+          solution,
+          when: dateFormat(created_at),
+          closed
+        });
+
+        setIsLoading(false);
+      });
+  }, []);
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
     <VStack flex={1} bg="gray.700">
       <Header title="Solicitação" />
-      <Text color="white">{orderId}</Text>
+      <HStack bg="gray.500" justifyContent="center" p={4}>
+        {order.status === "closed" ? (
+          <CircleWavyCheck size={22} color={colors.green[300]} />
+        ) : (
+          <Hourglass size={22} color={colors.secondary[700]} />
+        )}
+        <Text
+          fontSize="sm"
+          color={
+            order.status === "closed"
+              ? colors.green[300]
+              : colors.secondary[700]
+          }
+          ml={2}
+          textTransform="uppercase"
+        ></Text>
+      </HStack>
     </VStack>
   );
 }
